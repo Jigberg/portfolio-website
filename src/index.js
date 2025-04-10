ad9a8da3635738d2d6d384a9e344c2b34cd8aca7
Jigberg <lukas.jigberg@gmail.com>
2025-04-10 11:59:10 +0200

    HashRouter test

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './pages/App';
-import About from './pages/About';
-import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import PageNotFound from './pages/PageNotFound';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./pages/App";
+import About from "./pages/About";
+import PageNotFound from "./pages/PageNotFound";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-const router = createBrowserRouter([
-  { path:"/", element: <App /> },
-  { path:"/about", element: <About /> },
-  { path:"*", element: <PageNotFound /> },
-]);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router = {router} />
-  </React.StrictMode>
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  </HashRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
