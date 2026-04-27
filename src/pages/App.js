8f6c462412b9cde14bb3a65dfa0793a17802bf0c
Lukas Jigberg <lukas.jigberg@gmail.com>
2026-04-27 18:38:27 +0200

    cleanup

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,8 +1,6 @@
-import logo from "../logo.svg";
 import linkedinIcon from "../Subtract(1).svg";
 import githubIcon from "../Subtract(2).svg";
 import mailIcon from "../Subtract.svg";
-import planet from "../planetIMG.png";
 import { Suspense, useRef } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { useGLTF, OrbitControls, Environment, Stars } from "@react-three/drei";
@@ -90,7 +88,7 @@ function ModelRat() {
   );
 }
 
-function ModelSaturn() {
+function ModelMoon() {
   //https://skfb.ly/6Tswr
   const { scene } = useGLTF("/MoonT1.glb");
   const ref = useRef();
@@ -117,14 +115,15 @@ function ModelSaturn() {
 
 function ModelShuttle() {
   //https://skfb.ly/6Tswr
-  const { scene } = useGLTF("/space_shuttle.glb");
+  const { scene } = useGLTF("/sat.glb");
   const ref = useRef();
 
   const clonedScene = useMemo(() => SkeletonUtils.clone(scene), [scene]);
 
   useFrame(() => {
     if (ref.current) {
-      ref.current.rotation.y = (ref.current.rotation.y + 0.005) % (Math.PI * 2);
+      ref.current.position.y = -1 + Math.sin(Date.now() * 0.001) * 0.03;
+      //ref.current.rotation.y = (ref.current.rotation.y + 0.005) % (Math.PI * 2);
     }
   });
 
@@ -133,8 +132,8 @@ function ModelShuttle() {
       ref={ref}
       object={clonedScene}
       scale={0.2}
-      rotation={[0.7, 0, 0.2]}
-      position={[2, 1.5, -7]}
+      rotation={[-0.8, 0.5, 0.2]}
+      position={[2.5, -1, -3.5]}
     />
   );
 }
@@ -164,110 +163,10 @@ function ModelShip() {
   );
 }
 
-function ModelRing() {
-  //https://skfb.ly/JIPA
-  const { scene } = useGLTF("/rockRing.glb");
-  const ref = useRef();
 
-  const clonedScene = useMemo(() => SkeletonUtils.clone(scene), [scene]);
-
-  useFrame(() => {
-    if (ref.current) {
-      //ref.current.position.y = -1.0 + Math.sin(Date.now() * 0.001) * 0.03;
-      ref.current.rotation.y =
-        (ref.current.rotation.y + 0.0005) % (Math.PI * 2);
-    }
-  });
-
-  return (
-    <primitive
-      ref={ref}
-      object={clonedScene}
-      scale={0.1}
-      rotation={[0, 0, 0]}
-      //rotation={[0.6, 1.3, -0.8]}
-      position={[-11, -4.0, -15]}
-    />
-  );
-}
-
-function ModelRockPlanet() {
-  //https://skfb.ly/JIPA
-  const { scene } = useGLTF("/rockPlanet.glb");
-  const ref = useRef();
-
-  const clonedScene = useMemo(() => SkeletonUtils.clone(scene), [scene]);
-
-  useFrame(() => {
-    if (ref.current) {
-      //ref.current.position.y = -1.0 + Math.sin(Date.now() * 0.001) * 0.03;
-      ref.current.rotation.y =
-        (ref.current.rotation.y + 0.0002) % (Math.PI * 2);
-    }
-  });
-
-  return (
-    <primitive
-      ref={ref}
-      object={clonedScene}
-      scale={0.1}
-      rotation={[0.0, 0.1, -0.4]}
-      position={[-11, -4.0, -15]}
-    />
-  );
-}
-
-function CameraLook() {
-  const { camera } = useThree();
-
-  useFrame((state) => {
-    const t = state.clock.getElapsedTime();
-    camera.rotation.y = Math.sin(t * 0.1) * 0.07;
-  });
-
-  return null;
-}
 
 function App() {
-  const [text, setText] = useState("Welcome");
-  const [altFont, setAltFont] = useState(false);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      // trigger just before the pop
-      setTimeout(() => {
-        setAltFont(true);
-
-        // revert back quickly after pop
-        setTimeout(() => setAltFont(false), 200);
-      }, 7680); // 96% of 8000ms
-    }, 8000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    const hasVisited = localStorage.getItem("visited");
-
-    const responses = [
-      ", good to see you!",
-      ", glad you're here!",
-      ", long time no see!",
-      ", glad you're back!",
-      ", hey again!",
-    ];
-
-    if (hasVisited) {
-      const random = responses[Math.floor(Math.random() * responses.length)];
-
-      setText(random);
-    } else {
-      setText("");
-      localStorage.setItem("visited", "true");
-    }
-  }, []);
-
-  const words = ["Welcome", "Developer", "UI Designer", "Full Stack"];
+ const words = ["Software Developer", "UI Designer", "Engineer", "Certified Nerd"];
 
   const [displayed, setDisplayed] = useState("");
   const [wordIndex, setWordIndex] = useState(0);
@@ -307,7 +206,7 @@ function App() {
 
           timeout = setTimeout(() => typeWord(words[nextIndex]), 400);
         }
-      }, 120); // delete speed (faster than typing)
+      }, 60); // delete speed (faster than typing)
     };
 
     // initial delay before first word starts
@@ -323,99 +222,90 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="text-content">
-          <div className="welcome">
-            <h1>
-              Welcome
-              <span className="font-main">
-                <span className="underscore">_</span>
-              </span>
-            </h1>
-          </div>
-          <div className="description">
-            <p>
-              {">"} i'm Lukas
-            </p>
-            <p>
-              {">"} <span style={{ color: "#5eabbe" }}>{displayed}</span>
-            </p>
-            <p>
-              {">"} <span style={{ color: "#4288c2" }}>Developer</span>{" "}
-              <span style={{ color: "#b34cb3" }}>UI Designer</span>{" "}
-              <span style={{ color: "#d3934a" }}>Engineer</span>
-            </p>
-          </div>
+      <div className="text-content">
+        <div className="welcome">
+          <h1>
+            Welcome
+            <span className="font-main">
+              <span className="underscore">_</span>
+            </span>
+          </h1>
         </div>
-        <div className="bottom-content">
-          <div className="contact-info">
-            <a
-              className="linkIcon"
-              href="https://linkedin.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={linkedinIcon} alt="LinkedIn" className="social-icon" />
-            </a>
-            <a
-              className="linkIcon"
-              href="https://github.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={githubIcon} alt="GitHub" className="social-icon" />
-            </a>
-            <a
-              className="linkIcon"
-              href="https://mail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={mailIcon} alt="Mail" className="social-icon" />
-            </a>
-          </div>
-
-          <div className="copyright">
-            <p style={{ margin: 0 }}>
-              © {new Date().getFullYear()} Lukas Jigberg
-            </p>
-          </div>
+        <div className="description">
+          <p>{">"} i'm Lukas</p>
+          <p>
+            {">"} <span style={{ color: "#4288c2" }}>Developer</span>{" "}
+            <span style={{ color: "#b34cb3" }}>UI Designer</span>{" "}
+            <span style={{ color: "#d3934a" }}>Engineer</span>
+          </p>
         </div>
-        <div className="canvas-container">
-          <Canvas
-            camera={{ position: [0, 0, 0], fov: 90 }}
-            gl={{ powerPreference: "high-performance" }}
+      </div>
+      <div className="bottom-content">
+        <div className="contact-info">
+          <a
+            className="linkIcon"
+            href="https://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <color attach="background" args={["#000000"]} />
-            <fog attach="fog" args={["#91bfdf", 1, 50]} />
-            <Suspense>
-              {/* <CameraLook /> */}
-              <ModelEarth />
-              <ModelRat />
-              <ModelShip />
-              <ModelRing />
-              <ModelRockPlanet />
-              <ModelSaturn />
-              <directionalLight
-                position={[2, 1.5, 5]}
-                intensity={2}
-                color={"#c3c8e6"}
-              />
-
-              <ambientLight intensity={0.1} />
-              <Stars
-                radius={300}
-                depth={300}
-                count={10000}
-                factor={5}
-                saturation={1}
-                fade
-              />
-              {/* <Environment preset="dawn" background /> */}
-            </Suspense>
-          </Canvas>
+            <img src={linkedinIcon} alt="LinkedIn" className="social-icon" />
+          </a>
+          <a
+            className="linkIcon"
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githubIcon} alt="GitHub" className="social-icon" />
+          </a>
+          <a
+            className="linkIcon"
+            href="https://mail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={mailIcon} alt="Mail" className="social-icon" />
+          </a>
         </div>
-      </header>
+
+        <div className="copyright">
+          <p style={{ margin: 0 }}>
+            © {new Date().getFullYear()} Lukas Jigberg
+          </p>
+        </div>
+      </div>
+      <div className="canvas-container">
+        <Canvas
+          camera={{ position: [0, 0, 0], fov: 90 }}
+          gl={{ powerPreference: "high-performance" }}
+        >
+          <color attach="background" args={["#000000"]} />
+          <fog attach="fog" args={["#91bfdf", 1, 50]} />
+          <Suspense>
+            {/* <CameraLook /> */}
+            <ModelEarth />
+            <ModelRat />
+            <ModelShip />
+            <ModelMoon />
+            <directionalLight
+              position={[2, 1.5, 5]}
+              intensity={2}
+              color={"#c3c8e6"}
+            />
+
+            <ambientLight intensity={0.1} />
+            <Stars
+              radius={300}
+              depth={300}
+              count={10000}
+              factor={5}
+              saturation={1}
+              fade
+            />
+            {/* <Environment preset="dawn" background /> */}
+          </Suspense>
+        </Canvas>
+      </div>
     </div>
   );
 }
